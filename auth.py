c050bf908052d69576cd15e826b11b77318d9bcf
JuliaSavochkina <Julie-Webster@ya.ru>
2021-12-01 09:58:10 +0300

    added is_allowed method

--- auth.py
+++ auth.py
@@ -1,15 +1,28 @@
-def is_allowed():
+import yaml
+
+
+def is_allowed(login: str, password: str) -> bool:
     """
     проверяет допуск пользователя по логину и паролю переданному в теле запроса в виде json
     :return:
     """
-    pass
+    auth_given = {'login': login, 'pass': password}
+    auth_known = []
+    with open('auth.yml') as config:
+        auth_data = yaml.load(config, Loader=yaml.FullLoader)
+    for each in auth_data.values():
+        auth_known.append(each)
+    return auth_given in auth_known
 
 
 def goto_shave():
     """
-    при наличии доступа редиректит пользователя на ендпоинт /shave, со всемми гет параметрами, которые были в ссылке
-    и проставляет ему идентификтор в куку. Если доступа нет, возвращает сообщение, что допуск запрещен
+    редиректит пользователя на ендпоинт /shave, со всемми гет параметрами, которые были в ссылке
+    и проставляет ему идентификтор в куку
     :return:
     """
     pass
+
+
+if __name__ == '__main__':
+    is_allowed('aaa', '111')
